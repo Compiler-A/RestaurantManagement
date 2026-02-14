61a990f0eb6738d7041442e3948d5c3e8557e399
Compiler <alimoussajob@gmail.com>
2026-02-14 16:40:38 +0200

    update Connection

--- DataLayerRestaurant/clsDataAccessLayer.cs
+++ DataLayerRestaurant/clsDataAccessLayer.cs
@@ -2,7 +2,7 @@
 {
     public class clsDataAccessLayer
     {
-        public static readonly string ConnectionString = "Server=localhost;Database=RestaurantManager;User Id=sa;Password=sa123456;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;";
+        public static readonly string ConnectionString = "Server=COMPILER;Database=RestaurantManager;User Id=sa;Password=sa123456;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;";
 
         public static readonly int Rows = 10;
     }
