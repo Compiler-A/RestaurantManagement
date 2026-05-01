39cc318b70a1c3c3fd15f2b930c56ae2cc42f66d
Compiler-A <alimoussajob@gmail.com>
2026-05-01 16:16:51 +0300

    Add: CreateSqlRecords.cs

---
+++ DataLayerRestaurant/CreateSqlRecords.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient.Server;
+using System.Data;
+
+
+namespace DataLayerRestaurant
+{
+    public static class CreateSqlRecords
+    {
+        public static IEnumerable<SqlDataRecord> CreateSqlRecord(IEnumerable<int> ids)
+        {
+            var metaData = new[]
+            {
+                new SqlMetaData("Id", SqlDbType.Int)
+            };
+
+            foreach (var id in ids)
+            {
+                var record = new SqlDataRecord(metaData);
+                record.SetInt32(0, id);
+                yield return record;
+            }
+        }
+    }
+}
+
+
